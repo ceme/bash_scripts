011e4c29f440d0340bdf36ec1d25db56798210b9
C McKeegan <ceme@cemedigital.com>
2011-07-31 16:25:05 -0700

    More like original : http://answers.google.com/answers/threadview/id/276934.html

--- check_url.sh
+++ check_url.sh
@@ -4,7 +4,7 @@ function check {
 	if [ $? -ne 0 ] ; then
 		echo "Error occurred getting URL $1;"
 		if [ $? -eq 6 ]; then
-			echo "Unable to reslove host"
+			echo "Unable to resolve host"
 		fi
 		if [ $? -eq 7 ]; then
 			echo "Unable to contact host"
@@ -12,6 +12,5 @@ function check {
 		exit 1
 	fi
 }
-curl -s -o "~/check_url_out.txt" $1
+curl -s -o "/dev/null" $1
 check;
-echo 'got it'
